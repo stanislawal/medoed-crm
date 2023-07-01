4b6ac0cbda661db86f1bfc481bc544c1efdcb0eb
andrey <golovin.andrey.27@gmail.com>
2023-07-01 14:44:08 +0300

    change types

---
+++ database/migrations/2023_07_01_143805_update_projects_table_change_type_collumn.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateProjectsTableChangeTypeCollumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function(Blueprint $table){
+            $table->text('dop_info')->change();
+            $table->decimal('price_client', 10, 2)->change();
+            $table->text('dop_info')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
