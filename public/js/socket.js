a93cf627c2e1656b373feae153d154a58196b050
andrey <golovin.andrey.27@gmail.com>
2023-07-29 14:37:53 +0300

    fix

--- public/js/socket.js
+++ public/js/socket.js
@@ -3,10 +3,13 @@ var __webpack_exports__ = {};
 /*!***************************************!*\
   !*** ./resources/js/socket/socket.js ***!
   \***************************************/
-window.socket = io.connect('http://192.168.11.129:7000');
+window.socket = io.connect('http://localhost:7000');
 window.getSocket = function () {
   return window.socket;
 };
+window.socket.on("connect", function () {
+  console.log("WebSocket connect");
+});
 
 // если получено событие PushNotification, вызываем функцию обновлени уведомслений
 window.socket.on('PushNotification', function (e) {
--- resources/js/socket/socket.js
+++ resources/js/socket/socket.js
@@ -1,10 +1,14 @@
-window.socket = io.connect('http://192.168.11.129:7000');
+window.socket = io.connect('http://localhost:7000');
 
 window.getSocket = function () {
     return window.socket;
 };
 
+window.socket.on("connect", () => {
+    console.log("WebSocket connect");
+});
+
 // если получено событие PushNotification, вызываем функцию обновлени уведомслений
 window.socket.on('PushNotification', (e) => {
     window.updateNotificationList();
-})
+});
