f26ce71d85351e92d5c092122c56d88155b5e757
stanislavalekseev <stas.2810104@gmail.com>
2023-08-10 10:14:50 +0300

    fix

--- resources/views/Render/Notifications/notification_list.blade.php
+++ resources/views/Render/Notifications/notification_list.blade.php
@@ -112,9 +112,9 @@
                         <div class="description">
                             <div class="text-notify">
                                 <span>Изменение в базе статей: <br></span>
-                                <div> {!! $item['message'] ?? '' !!}</div>
+                                <div> {!! $item['message'] !!}</div>
                                 <a href="{{ route('article.index', ['article' => $item['articles']['article']]) }}"
-                                   class="text-primary">{{ $item['articles']['article'] }}</a></div>
+                                   class="text-primary">{{ $item['articles']['article'] ?? null}}</a></div>
                             <div class="time">{{ $item['date_time'] }}</div>
                         </div>
                         <div class="browse"
