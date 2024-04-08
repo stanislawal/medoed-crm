12012d6e9f0c56a9040973e5376f1713a0a5ff76
andrey <golovin.andrey.27@gmail.com>
2024-04-08 14:50:05 +0300

    feat: update indicator for report author

--- resources/views/report/author/author_list.blade.php
+++ resources/views/report/author/author_list.blade.php
@@ -140,6 +140,14 @@
                             <div class="text-12 nowrap-dot">Выплачено:</div>
                         </div>
                     </div>
+
+                    <div class="col-12 col-sm-6 col-xl-4 mb-2">
+                        <div class="px-3 py-2 shadow border bg-white rounded">
+                            <div class="text-24">
+                                <strong>{{number_format($remainderDuty->sum('remainder_duty'), 2, '.', ' ')  }}</strong></div>
+                            <div class="text-12 nowrap-dot">Переносящийся долг:</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-12 col-md-3">
