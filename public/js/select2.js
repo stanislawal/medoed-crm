056b4873eabe993a844e0ec01f70312aae2b847f
Andrey <golovin.andrey.27@gmail.com>
2024-12-17 19:00:47 +0300

    feat: update

--- public/js/select2.js
+++ public/js/select2.js
@@ -5,7 +5,7 @@ var __webpack_exports__ = {};
   \*********************************/
 $(document).ready(function () {
   console.log('123');
-  $.fn.select2.defaults.set("theme", "bootstrap");
+  // $.fn.select2.defaults.set("theme", "bootstrap");
   $('.select-2').select2();
 });
 /******/ })()
--- resources/js/select2.js
+++ resources/js/select2.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     console.log('123')
-    $.fn.select2.defaults.set("theme", "bootstrap");
+    // $.fn.select2.defaults.set("theme", "bootstrap");
     $('.select-2').select2();
 });
 
